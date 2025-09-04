61eaf63f44932b0fcf2a314b2d1810368f52f5f9
Smith-B04 <smith.b04@student.lmas.ac.uk>
2025-09-04 11:25:24 +0100

    Sprint 1.1
    Added Player Character, tile map and began character movement code.

---
+++ NEA Game 2026/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+	public float speedModifer;
+	public float jumpModifier;
+	public GameObject playerCharacter;
+	private Rigidbody2D rb;
+	private bool sprint;
+
+	// Use this for initialization
+	void Start () {
+		rb = playerCharacter.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.A)) {
+			playerCharacter.transform.position = new Vector3 (playerCharacter.transform.position.x - 0.1f, playerCharacter.transform.position.y, playerCharacter.transform.position.z);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			playerCharacter.transform.position = new Vector3 (playerCharacter.transform.position.x + 0.1f, playerCharacter.transform.position.y, playerCharacter.transform.position.z);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.AddForce (new Vector3 (0, jumpModifier, 0));
+		}
+	}
+}
