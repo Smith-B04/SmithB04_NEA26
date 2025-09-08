e4a4ab3715c1dd777f9a6c10e0d924b30de19e0b
TheSmithason <smith.b04@student.lmas.ac.uk>
2025-09-08 12:44:29 +0100

    Sprint 1.2
    Added slowed air movement and health bar. But health bar isn't working because takeDamage function isn't running 😠

--- NEA Game 2026/Assets/Scripts/MovementScript.cs
+++ NEA Game 2026/Assets/Scripts/MovementScript.cs
@@ -1,30 +1,90 @@
-﻿using System.Collections;
+﻿using Microsoft.Unity.VisualStudio.Editor;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using static System.Net.Mime.MediaTypeNames;
 
 public class MovementScript : MonoBehaviour {
 
-	public float speedModifer;
-	public float jumpModifier;
-	public GameObject playerCharacter;
-	private Rigidbody2D rb;
-	private bool sprint;
-
-	// Use this for initialization
-	void Start () {
-		rb = playerCharacter.GetComponent<Rigidbody2D> ();
-	}
 	
+	public GameObject playerCharacter;
+    public UnityEngine.UI.Image healthBar;
+    private Rigidbody2D rb;
+    public float speedModifier;
+	public bool sprinting;
+	private bool isGrounded;
+    public float jumpModifier;
+    public int maxHealth;
+    private int health;
+
+
+    // Use this for initialization
+    void Start () {
+        health = maxHealth;
+		rb = playerCharacter.GetComponent<Rigidbody2D> ();
+		sprinting = false;
+    }
+
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update() {
+        TakeDamage(5, "hello");
+    }
+
+
+    void FixedUpdate () {
 		if (Input.GetKey (KeyCode.A)) {
-			playerCharacter.transform.position = new Vector3 (playerCharacter.transform.position.x - 0.1f, playerCharacter.transform.position.y, playerCharacter.transform.position.z);
-		}
+            playerCharacter.transform.position = new Vector3(
+                (isGrounded) ? (
+                Input.GetKey(KeyCode.LeftShift) ?
+                (playerCharacter.transform.position.x - (float)(1.5 * speedModifier)) :
+                (playerCharacter.transform.position.x - speedModifier)
+                ) :
+                ((playerCharacter.transform.position.x - (float)(0.5 * speedModifier))),
+                playerCharacter.transform.position.y,
+                playerCharacter.transform.position.z
+                );
+        }
 		if (Input.GetKey (KeyCode.D)) {
-			playerCharacter.transform.position = new Vector3 (playerCharacter.transform.position.x + 0.1f, playerCharacter.transform.position.y, playerCharacter.transform.position.z);
+            playerCharacter.transform.position = new Vector3(
+                (isGrounded) ? (
+                Input.GetKey(KeyCode.LeftShift) ?
+                (playerCharacter.transform.position.x + (float)(1.5 * speedModifier)) :
+                (playerCharacter.transform.position.x + speedModifier)
+                ) :
+                ((playerCharacter.transform.position.x + (float)(0.5 * speedModifier))),
+                playerCharacter.transform.position.y,
+                playerCharacter.transform.position.z
+                );
+        }
+    }
+
+    private void TakeDamage(int damage, string /*change to set type*/ damageType)
+    {
+        health -= damage/* multiply by damage resistances dictionary.damageType */;
+        healthBar.fillAmount = health/maxHealth;
+    }
+
+    void OnJump()
+	{
+        rb.AddForce(new Vector3(0, jumpModifier, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
 		}
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			rb.AddForce (new Vector3 (0, jumpModifier, 0));
-		}
-	}
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
 }
